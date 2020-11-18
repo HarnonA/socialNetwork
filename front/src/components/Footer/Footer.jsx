574dd7dd0598b13393a6faea087f145d5bacdc2d
Harnon Anizio <harnonanizio@gmail.com>
2020-11-17 23:07:55 -0300

    delete brand related

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import './styles.css';
 
-import logo from '../assets/logo.png'
 import instagram from '../assets/instagram.svg'
 import facebook from '../assets/facebook.svg'
 
@@ -14,12 +13,10 @@ function Footer() {
                 <p>Phone: +55 11 123456789</p>
             </div>
             <div className="imageLogo">
-                {/* <p>VeganHive</p> */}
-                <img src={logo} alt="veganhive" />
+                
             </div>
             <div className="social">
-            <a href="https://www.instagram.com/veganhive/"><img src={instagram} alt="instagram" width={21} /></a>
-                <a href="https://www.facebook.com/veganhive/"><img src={facebook} alt="facebook" /></a>
+            
 
             </div>
         </div>
